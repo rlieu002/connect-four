3c368b9cf4c10b912c57c5cb93ca8a049bf07af4
Richard <richard.lieu@synctree.com>
2020-02-28 09:58:59 -0800

    format and deploy

--- build/precache-manifest.86ab901fea12e504ac551b645743a874.js
+++ build/precache-manifest.86ab901fea12e504ac551b645743a874.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e7393de542feb3e8be17236317fa7bdc",
+    "revision": "35c98f089163c779ac8793143026eb51",
     "url": "/index.html"
   },
   {
-    "revision": "d9f0ed19881614547a82",
+    "revision": "21533035cdb0e5c21e13",
     "url": "/static/css/main.a7a33d0b.chunk.css"
   },
   {
-    "revision": "94b6e9ac0e0dddda17ff",
-    "url": "/static/js/2.8bfe02b8.chunk.js"
+    "revision": "6034dd1b485ac017e12e",
+    "url": "/static/js/2.6632816b.chunk.js"
   },
   {
     "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
-    "url": "/static/js/2.8bfe02b8.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.6632816b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d9f0ed19881614547a82",
-    "url": "/static/js/main.380b031c.chunk.js"
+    "revision": "21533035cdb0e5c21e13",
+    "url": "/static/js/main.b5c89b78.chunk.js"
   },
   {
     "revision": "7a62ab0c16e3a19cbf0a",
